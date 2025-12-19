409335ec81db1d296ca33b9c7322a1b3c9fc9d8c
Safwan khan <safwankhan010101@gamil.com>
2025-12-19 08:39:47 +0500

    Initial Commit

---
+++ ticTacToeApp/ticTacToeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ticTacToeAppApp.swift
+//  ticTacToeApp
+//
+//  Created by Apple Orchard on 19/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ticTacToeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ticTacToeAppTests/ticTacToeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ticTacToeAppTests.swift
+//  ticTacToeAppTests
+//
+//  Created by Apple Orchard on 19/12/2025.
+//
+
+import Testing
+@testable import ticTacToeApp
+
+struct ticTacToeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
